d72d148afbf8378c2286c0c762f5ca7c4ad1ae91
Borys35 <boryskac10@gmail.com>
2026-03-26 20:14:24 +0100

    finish project

--- gamma.cpp
+++ gamma.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <mpi.h>
-#include <ncurses.h>
 #include <cmath>
 
 int main(int argc, char *argv[])
@@ -20,20 +19,26 @@ int main(int argc, char *argv[])
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // std::cout << "Proces " << world_rank << " odebral wiadomosc " << n << std::endl;
     int start = world_rank * (n / (world_size));
     int end = (world_rank + 1) * (n / (world_size));
-    int part_sum = 0;
-    for (int i = start; i < end; i++) {
-        // part_sum += 1.0 / i;
-        std::cout << "Proces " << world_rank << ", i=" << i << std::endl;
-        part_sum += i;
+    int remainder = n % world_size;
+    if (world_rank < remainder) {
+        start += world_rank;
+        end += world_rank + 1;
+    } else {
+        start += remainder;
+        end += remainder;
     }
-    int sum = 0;
-    MPI_Reduce(&part_sum, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    double part_sum = 0;
+    for (int i = start; i < end; i++) {
+        part_sum += 1.0 / (i + 1);
+    }
+    double sum = 0;
+    MPI_Reduce(&part_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (world_rank == 0) {
-        std::cout << "Suma wszystkich elementow: " << sum << std::endl;
+    if (world_rank == 0) { // tylko "serwer" oblicza wynik i go wyswietla
+        double result = sum - log(n);
+        std::cout << "Gamma: " << result << std::endl; // stała Eulera-Mascheroniego = 0,57721566
     }
 
     MPI_Finalize();
